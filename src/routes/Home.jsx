c3f1cfdb744110b1249769983feed099dc43f8f3
OKeylo <zabaev228@gmail.com>
2023-11-13 15:46:18 +0500

    feat(routes\Home): add username and phone display in Car

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,22 +1,57 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react'
-import './Home.css'
 import Header from '../components/Header';
+import Phone from '../assets/phone.png';
+import User from '../assets/user.png';
 
 function Car({car}) {
-    return (
-        <div className='car'>
-            <p className='carName'>
-                {car.mark} {car.model}, {car.year}
-            </p>
-            <p style={{fontSize: "20px", fontWeight: "600", marginBottom: "5%"}}>
-                {car.price} ₽
-            </p>
-            <p>Характеристики</p>
-            <p>{car.fuel} ({car.power} л.с.)</p>
-            <p>{car.mileage} км</p>
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    const getUser = async (id) => {
+      const user = await axios.get(`http://127.0.0.1:8000/user/${id}`);
+      return user.data;
+    };
+
+    getUser(car.author_id)
+    .then(user => {
+      setUser(user);
+    })
+    .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <div className='car'>
+      <p className='carName'>
+        {car.mark} {car.model}, {car.year}
+      </p>
+      <p style={{fontSize: "20px", fontWeight: "600", marginBottom: "5%"}}>
+        {car.price} ₽
+      </p>
+      <div style={{marginBottom: "5%"}}>
+        <p style={{fontWeight: "600"}}>Характеристики</p>
+        <p>{car.fuel} ({car.power} л.с.)</p>
+        <p>{car.mileage} км</p>
+      </div>
+      {user?
+        <div style={{display: "flex", flexDirection:"column"}}>
+          <div style={{display: "flex", alignItems: "center", gap: "5px"}}>
+            <img src={User} style={{height: "20px"}} alt='phone' />
+            <p>{user?.username}</p>
+          </div>
+          <div style={{display: "flex", alignItems: "center", gap: "5px"}}>
+            <img src={Phone} style={{height: "20px"}} alt='phone' />
+            <p>{user?.phone}</p>
+          </div>
+          
         </div>
-    )
+        :
+        <></>
+      }
+    </div>
+  )
 }
 
 function Home() {
@@ -33,13 +68,15 @@ function Home() {
   }, []);
 
   return (
-    <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+    <div>
       <Header />
-      <h2>Продажа автомобилей</h2>
-      <div className='carlist'>
-        {cars?.map((car, i) => {
-            return <Car key={i} car={car} />
-        })}
+      <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+        <h2>Продажа автомобилей</h2>
+        <div className='carlist'>
+          {cars?.map((car, i) => {
+              return <Car key={i} car={car} />
+          })}
+        </div>
       </div>
     </div>
   )
