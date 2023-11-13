1eebb89633126524d94f7b3755d3c144e2309f3e
OKeylo <zabaev228@gmail.com>
2023-11-13 07:32:07 +0500

    feat(routes\MyAccount): add deleteCar func

--- src/routes/MyAccount.jsx
+++ src/routes/MyAccount.jsx
@@ -3,21 +3,6 @@ import { useState, useEffect } from 'react'
 import Header from '../components/Header';
 import { useNavigate } from 'react-router-dom';
 
-function Car({car}) {
-  return (
-    <div className='car'>
-      <p className='carName'>
-        {car.mark} {car.model}, {car.year}
-      </p>
-      <p style={{fontSize: "20px", fontWeight: "600", marginBottom: "5%"}}>
-        {car.price} ₽
-      </p>
-      <p>Характеристики</p>
-      <p>{car.fuel} ({car.power} л.с.)</p>
-      <p>{car.mileage} км</p>
-    </div>
-  )
-}
 
 function MyAccount() {
   const navigate = useNavigate()
@@ -42,12 +27,51 @@ function MyAccount() {
     getCars();
   }, []);
 
+  const deleteCar = async (event, car_id) => {
+    event.preventDefault();
+  
+    await axios.delete(
+      `http://127.0.0.1:8000/cars/${car_id}`,
+      {
+        auth: {
+          username: user.username,
+          password: user.password
+        }
+      }
+    )
+    .then(response => {
+      setCars(cars.filter((car) => car.id != car_id));
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+
+  function Car({car}) {
+    return (
+      <div className='car'>
+        <p className='carName'>
+          {car.mark} {car.model}, {car.year}
+        </p>
+        <p style={{fontSize: "20px", fontWeight: "600", marginBottom: "5%"}}>
+          {car.price} ₽
+        </p>
+        <p>Характеристики</p>
+        <p>{car.fuel} ({car.power} л.с.)</p>
+        <p>{car.mileage} км</p>
+        <div style={{display: "flex", justifyContent: "center", marginTop: "5%"}}>
+          <button onClick={(event) => deleteCar(event, car.id)} className='remove_car'>Убрать объявление</button>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div>
       <Header />
       <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
-        <h2>Мой профиль</h2>
-        <h3>Мои данные</h3>
+        <h1 style={{fontSize: "32px"}}>Мой профиль</h1>
+        <h2>Мои данные</h2>
         <div style={{fontSize: "20px", marginBottom: "1%"}}>
           <p>Имя пользователя: {user.username}</p>
           <p>Номер телефона: {user.phone}</p>
